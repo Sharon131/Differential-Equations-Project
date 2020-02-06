7cb5026bfde820e7cdcc29df6aaab06dc91e9889
Sharon131 <pastula.magdalena@gmail.com>
2020-02-06 10:10:41 +0100

    Almost working difficult version.

--- projekt_easy.R
+++ projekt_easy.R
@@ -1,11 +1,11 @@
 
-matrix_A <- matrix(nrow = 3, ncol = 3)
+matrix_A <- matrix(nrow = 2, ncol = 2)
 
-matrix_A[,1] <- c(0, -1, 0)
-matrix_A[,2] <- c(-1, 3, -2)
-matrix_A[,3] <- c(0, -2, 2)
+matrix_A[,1] <- c(0, -1) # 0
+matrix_A[,2] <- c(-1, 3) # -2
+#matrix_A[,3] <- c(0, -2, 2)
 
-matrix_B  <- c(-20,0,0)
+matrix_B  <- c(-20,0)
 
 e1 <- function(x) {
   result <- -x+1
@@ -25,6 +25,9 @@ e3 <- function(x) {
   return(result)
 }
 
+print("Matrix B:")
+print(matrix_B)
+
 print("My matrix:")
 print(matrix_A)
 
@@ -38,6 +41,6 @@ funcs[,2] <- c(e2(x))
 funcs[,3] <- c(e3(x))
 
 x <- seq(0,2,0.1)
-y <- coeffs[1]*e1(x)+coeffs[2]*e2(x)+coeffs[3]*e3(x)
+y <- coeffs[1]*e1(x)+coeffs[2]*e2(x)
 
 plot(x, y, main="Function u(x)", ylab="u(x)", type="o", col="blue")
--- projekt_hard.R
+++ projekt_hard.R
@@ -5,18 +5,68 @@ l <- 2/(n-1)
 # Iloœæ linii prostych:
 m <- 2*(n-1)
 
-derivative <- function(f, x) {
+x <- seq(0,2,0.1)
+
+derivative2 <- function(f, x) {
   h <- 0.01
   return((f(x+h)-f(x-h))/(2*h))
 }
 
-integral <- function(f, x) {
+derivative <- function(Ei, x, i) {
+  h <- function(x, i){
+    if (i == 1) {
+      D_lower <- 0
+      D_upper <- 2*i/(n-1)
+      result <- integer(length(x[x>=D_lower & x <=D_upper]))+a[1]
+      result <- c(result, integer(length(x[x>D_upper])))
+      return(result)
+    } 
+    else if (i == n) {
+      D_lower <- 2*(i-2)/(n-1)
+      D_upper <- 2
+      result <- integer(length(x[x<D_lower]))
+      result <- c(result,integer(length(x[x>=D_lower & x <=D_upper]))+a[m])
+      
+      return(result)
+    } 
+    else{
+      D_lower <- 2*(i-2)/(n-1)
+      D_upper <- 2*i/(n-1)
+      D_middle <- 2*(i-1)/(n-1)
+      
+      values <- a[i+n-2]+integer(length(x[x>=D_lower & x<=D_middle]))
+      val <- a[i]+integer(length(x[x>D_middle & x<=D_upper]))
+      values <- c(values, val)
+    }
+    
+    result <- integer(length(x[x<D_lower]))
+    result <- c(result,values)
+    
+    result <- c(result, integer(length(x[x>D_upper])))
+    
+    return(result)  
+  }
+  return(h)
+}
+
+integral <- function(Ei, x, i) {
+  x1 <- -1/(sqrt(3))
+  x2 <- 1/(sqrt(3))
   
-  return(0)
+  integr <- Ei((x1+1)*2/(n-1), i)+Ei((x2+1)*2/(n-1), i)
+  return(integr*(n-1)/2)
+}
+
+integral2 <- function(f, x, D_middle) {
+  x1 <- -1/(sqrt(3))
+  x2 <- 1/(sqrt(3))
+  
+  integr <- f((x1+1)*2/(n-1))+f((x2+1)*2/(n-1))
+  return(integr*(n-1)/2)
 }
 
 x1 <- seq(0,2-l,l)
-x2 <- seq(1,2,l)
+x2 <- seq(l,2,l)
 
 
 a1 <- 1/(x2-x1)
@@ -32,12 +82,12 @@ b <- c(b2,b1)
 Ei <- function(x, i) {
   if (i == 1) {
     result <- a[1]*x + b[1]
-    result[result<0] <- 0
+    result[result<0 & result>1] <- 0
     return(result)
   } 
   else if (i == n) {
     result <- a[m]*x+b[m]
-    result[result<0] <- 0
+    result[result<0 & result>1] <- 0
     return(result)
   } 
   else{
@@ -45,8 +95,8 @@ Ei <- function(x, i) {
     D_upper <- 2*i/(n-1)
     D_middle <- 2*(i-1)/(n-1)
     
-    values <- a[i+n-2]*x[x>D_lower & x<D_middle] + b[i+n-2]
-    val <- a[i]*x[x>D_middle & x<D_upper] + b[i]
+    values <- a[i+n-2]*x[x>=D_lower & x<=D_middle] + b[i+n-2]
+    val <- a[i]*x[x>D_middle & x<=D_upper] + b[i]
     values <- c(values, val)
   }
   
@@ -58,64 +108,64 @@ Ei <- function(x, i) {
   return(result)
 }
 
-# dodaæ obliczanie macierzy A z ca³ek (f. B(ei, ei))
-# matrix_A <- matrix(nrow=n, ncol=n)
-matrix_A <- matrix(nrow = 3, ncol = 3)
+L <- function(Ei, i) {
+  return(-20*Ei(0, i))
+}
 
-matrix_A[,1] <- c(0, -1, 0)
-matrix_A[,2] <- c(-1, 3, -2)
-matrix_A[,3] <- c(0, -2, 2)
-
-#dodaæ obliczanie macierzy B z f. L(ei)
-matrix_B  <- c(-20,0,0)
-
-e1 <- function(x) {
-  result <- -x+1
-  result[result<0] <- 0
-  result[result>1] <- 0
+B <- function(u,v, i_u, i_v) {
+  u_ <- derivative(u,x,i_u)
+  v_ <- derivative(v,x,i_v)
+  
+  t <- function(x) {
+    return(k(x)*u_(x, i_u)*v_(x,i_v))
+  }
+  
+  D_middle = (i_u+i_v-2)/(n-1)
+  result <- -u(0, i_u)*v(0, i_v)+integral2(t, x, D_middle) #ca³ka z k(x)*u'(x)*v'(x)
   return(result)
-}
-
-e2 <- function(x) {
-  result <- x
-  result[result>=1] <- (-result[result>=1])+2
-  return(result)
-}
-
-e3 <- function(x) {
-  result <- x-1
-  result[result<0] <- 0
-  return(result)
-}
-
-L <- function(v) {
-  return(-20*v(0))
-}
-
-B <- function(u,v) {
-  result <- -u(0)*v(0) #ca³ka z k(x)*u'(x)*v'(x)
 }
 
 k <- function(x) {
-  if (x <= 1 && x >=0) {
-    return(1)
-  } else if (x <= 2) {
-    return(2)
-  } else {
-    return(0)
-  }
+  result <- integer(length(x[x<=1 & x>=0]))+1
+  result <- c(result, integer(length(x[x>1 & x<=2]))+2)
+  
+  return(result)
 }
 
-x <- seq(0,2,0.1)
+L_matrix <- L(Ei, 1)
 
-print("My matrix:")
-print(matrix_A)
+for (i in 2:(n-1)) {
+  L_matrix <- c(L_matrix, L(Ei, i))
+}
 
-coeffs <- solve(matrix_A, matrix_B)
+# Tworzenie macierzy B
+B_matrix <- matrix(nrow=n-1, ncol=n-1)
+
+for (i in 1:(n-1)){
+  buffer <- B(Ei, Ei, i, 1)
+  for (j in 2:(n-1)){
+    #B_matrix[i][j] <- B(Ei, Ei, i, j)
+    buffer <- c(buffer, B(Ei, Ei, i, j))
+  }
+  print(buffer)
+  B_matrix[,i] <- buffer
+}
+
+print("L_matrix:")
+print(L_matrix)
+
+print("B_matrix:")
+print(B_matrix)
+
+coeffs <- solve(B_matrix, L_matrix)
 print("Solution:")
 print(coeffs)
 
 x <- seq(0,2,0.1)
-y <- coeffs[1]*e1(x)+coeffs[2]*e2(x)+coeffs[3]*e3(x)
+y <- coeffs[1]*Ei(x, 1)
+
+for (i in 2:(n-1)) {
+  y <- y + coeffs[i]*Ei(x, i)
+}
 
 plot(x, y, main="Function u(x)", ylab="u(x)", type="o", col="blue")
